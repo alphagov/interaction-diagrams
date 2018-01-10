01adf79b94bb82ad23f06172fe9983e014ac6054
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-01-10 12:14:30 +0000

    remove unused commands, clarify loopback devices/os platforms

--- src/interactive_tcpdump_network_traffic_writer.rb
+++ src/interactive_tcpdump_network_traffic_writer.rb
@@ -4,8 +4,8 @@ class InteractiveTcpdumpNetworkTrafficWriter
   end
 
   def write_network_traffic_to(tcpdump_output_file_path)
-    loopback_device = 'lo0'
-    loopback_device = 'lo' if !((RUBY_PLATFORM =~ /linux/).to_i).zero?
+    loopback_device = 'lo0' # macos loopback device
+    loopback_device = 'lo' if !((RUBY_PLATFORM =~ /linux/).to_i).zero? # linux loopback device
     SystemCommandExecutor.run_tcpdump_and_wait_for_key_press(loopback_device, tcpdump_output_file_path, @ports, 'Recording network traffic. Press ENTER to stop...')
   end
 end
